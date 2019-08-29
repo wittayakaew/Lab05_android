d74c52cc79e5e39c81500a880f5319d8c23c44d5
wittayakaew <59160039@go.buu.ac.th>
2019-08-29 15:57:42 +0700

    add layout login

--- app/src/main/java/com/example/lab05/LoginFragment.kt
+++ app/src/main/java/com/example/lab05/LoginFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.lab05.databinding.FragmentLoginBinding
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,13 +20,13 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class LoginFragment : Fragment() {
-
+    private lateinit var binding : FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+        return binding.root
     }
 
 
