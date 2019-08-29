be02a823467bf6c247a40d956e3f40cef2419d58
wittayakaew <59160039@go.buu.ac.th>
2019-08-29 17:24:13 +0700

    add option menu

--- app/src/main/java/com/example/lab05/CarParkFragment.kt
+++ app/src/main/java/com/example/lab05/CarParkFragment.kt
@@ -3,12 +3,13 @@ package com.example.lab05
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.lab05.databinding.FragmentCarParkBinding
+import java.util.zip.Inflater
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -34,7 +35,7 @@ class CarParkFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate<FragmentCarParkBinding>(inflater, R.layout.fragment_car_park,
             container, false)
-
+        setHasOptionsMenu(true)
         binding.buttonSlot1.setOnClickListener(){
             slot = 1
             showview(it,binding)
@@ -68,6 +69,15 @@ class CarParkFragment : Fragment() {
         binding.car2 = this@CarParkFragment.car2
         binding.car3 = this@CarParkFragment.car3
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.option_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,view!!.findNavController())||super.onOptionsItemSelected(item)
     }
 
     private fun showview(view:View,binding: FragmentCarParkBinding) {
---
+++ app/src/main/java/com/example/lab05/InfoFragment.kt
@@ -0,0 +1,34 @@
+package com.example.lab05
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.lab05.databinding.FragmentInfoBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class InfoFragment : Fragment() {
+    private lateinit var binding : FragmentInfoBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_info,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+}
